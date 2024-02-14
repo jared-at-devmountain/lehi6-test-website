c4da9075f479fd5dc91e815e743b3895556d0bbe
Jared Collier <collierjared@gmail.com>
2024-02-14 16:02:59 -0700

    does backend endpoint call

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
 
+  let [arr, setArr] = useState([])
+
+  useEffect(() => {
+    axios.get('/stuff')
+    .then((response) => {
+      console.log(response.data)
+      setArr(response.data)
+    })
+  }, [])
 
   return (
     <>
       hi mom
+      {
+        arr.map((num) => {
+          return <p>{num}</p>
+        })
+      }
     </>
   )
 }
